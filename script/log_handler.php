f763698cb7a41b45bdd223dda954433098d891e0
Thomas Crone Larsen <thom802b@edu.eal.dk>
2017-11-08 12:15:02 +0100

    Proof at vi kan læse .log filen med PHP

--- script/log_handler.php
+++ script/log_handler.php
@@ -1,8 +1,19 @@
 <?php
 //This should be handled as a script. This will be implementet later
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+echo "This is to test reading log file\n";
+$file = fopen('php_error.log', 'r');
 
+$counter = 0;
+
+while (true) {
+    $counter++;
+    $line_in_file = fgets($file);
+    if (!$line_in_file) {
+        //reached end of file
+        break;
+    }
+    if(($line_in_file) >= 1) {
+        //continue;
+    }
+    echo "Line " .$counter . " - " . $line_in_file . "\n";
+}
