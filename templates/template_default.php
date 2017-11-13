07a1fdf8f872f223c41a08dd8ca319e7243d58b6
MCVDIEU <mathiascvdieu@hotmail.com>
2017-11-13 10:26:26 +0100

    templetes updates 2

--- templates/template_default.php
+++ templates/template_default.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>[%pagetitle%]</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <title>[%pagetitle%]</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="/css/shared_css.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">	
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	[%header%]
-</head>
-<body>
-	<div id="wrapper-content" class="container">
-		[%result_msg%]
-		[%content%]
-		[%footer%]
-	</div>
-</body>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">	
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        [%header%]
+    </head>
+    <body>
+        [%menu%]
+        <div id="wrapper-content" class="container">
+
+            [%result_msg%]
+            [%content%]
+            [%footer%]
+        </div>
+    </body>
 </html>
