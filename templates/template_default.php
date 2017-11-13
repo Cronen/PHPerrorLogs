b1e614c376a06da4d46fe8d92ce264c04a4da3a4
MCVDIEU <mathiascvdieu@hotmail.com>
2017-11-13 10:23:55 +0100

    templetes updates

--- templates/template_default.php
+++ templates/template_default.php
@@ -4,20 +4,17 @@
 	<title>[%pagetitle%]</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="/css/shared_css.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="/css/profile.css">
 	[%header%]
 </head>
 <body>
-	<div id="wrapper-menu">
-		[%menu%]
-	</div>
-	<div id="wrapper-content">
+	<div id="wrapper-content" class="container">
+		[%result_msg%]
 		[%content%]
+		[%footer%]
 	</div>
-	[%footer%]
 </body>
 </html>
