c5ae7fb7dadecbb755b870169dbc9f9408740a5a
MCVDIEU <mathiascvdieu@hotmail.com>
2017-11-13 12:59:54 +0100

    page content added

--- Pages/overview.php
+++ Pages/overview.php
@@ -1,3 +1,44 @@
 <?php
+$data = new db_md();
+$tablemkr = new table_md_class();
+?>
 
-echo "Hello bitches";
+<div class="container overview-table">
+    <div class="col-md-12">
+        <?php
+        $sql = "
+        SELECT 
+            fornavn,
+            efternavn, 
+            alder, 
+            adresse,
+            '' AS 'Handling'
+	FROM dummydata";
+        
+        $arrays = $data->makeArray($sql);
+        foreach ($arrays as $array) {
+            $row = $array;
+            
+            //Array med handlinger/tools/triggers
+            $tools = array();
+
+            //"slet postering" trigger
+            $tools[] = '<button class="btn-danger">Slet postering</button>';
+            
+            //"Udskyd postrings" trigger
+            $tools[] = '<button class="btn-warning">udskyd</button>';
+            
+            //"Gennemset" trigger
+            $tools[] = '<button class="trigger-btn btn-success"> Godkend</button>';
+
+            $row['Handling'] = implode(' ', $tools);
+
+            $finished[] = $row;
+        }
+        $html[] = $tablemkr->makeTable($finished);
+
+        //render
+        echo implode('', $html);
+        ?>
+    </div>
+</div>
