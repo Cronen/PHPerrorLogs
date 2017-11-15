df62bfa1c99bfcee486ac5b13d1b8ecfc0f86cf5
newnamtab <newnamtab@yahoo.dk>
2017-11-15 22:44:15 +0100

    log_handler so far.. still something wrong in arrays and classes

--- Pages/overview.php
+++ Pages/overview.php
@@ -1,44 +1,77 @@
-<?php
-$data = new db_md();
-$tablemkr = new table_md_class();
-?>
+<?php ?>
 
-<div class="container overview-table">
+<div class="container-fluid">
+    <div class="col-md-12">
+        <span>Vælg at sortere efter:</span>
+        <button>Level</button>
+        <button>Dato</button>
+        <button>Site</button>
+        <button>Vis alt</button>
+    </div>
+</div>
+
+<div class="container-fluid overview-table">
     <div class="col-md-12">
         <?php
-        $sql = "
-        SELECT 
-            fornavn,
-            efternavn, 
-            alder, 
-            adresse,
-            '' AS 'Handling'
-	FROM dummydata";
-        
+        //Instantiering af klasser
+        $data = new db_md();
+        $tablemkr = new table_md_class();
+
+        // Hent alle php_errors og tilføj til array
+        $sql = "SELECT * FROM php_error";
         $arrays = $data->makeArray($sql);
-        foreach ($arrays as $array) {
+
+        //gruppér på error_id
+        $erlvs = array();
+        foreach($arrays as $array) 
+        {
+            $erlvs[ $array['error_ID'] ][] = $array;
+        }
+
+        //indhent data ud fra sorteringsvalg
+        $table_sql = "
+                SELECT 
+                '' AS 'Handling',
+                error_ID AS ID,
+                error_date AS Dato,
+                php_error_level AS level,
+                error_msg AS Fejlmeddelse,
+                error_location AS URL,
+                error_file AS Fil,
+                error_line AS linje
+                FROM php_error";
+
+        //Lav tabel med indhentet data
+        $table_data = $data->makeArray($table_sql);  
+        
+        $finished = array();
+        foreach($table_data as $array) 
+        {
+            //Har navngivet error_id til ID, derfor bruger jeg her 'ID'
+            $error_id = $array['ID'];
+            
             $row = $array;
             
             //Array med handlinger/tools/triggers
             $tools = array();
-
+            
             //"slet postering" trigger
-            $tools[] = '<button class="btn-danger">Slet postering</button>';
+            $tools[] = '<button>Slet</button>';
+            //"Udskyd postering" trigger
+            $tools[] = '<button>Udskyd</button>';
+            //"Godkend postering" trigger
+            $tools[] = '<button>Godkend</button>';
             
-            //"Udskyd postrings" trigger
-            $tools[] = '<button class="btn-warning">udskyd</button>';
-            
-            //"Gennemset" trigger
-            $tools[] = '<button class="trigger-btn btn-success"> Godkend</button>';
-
             $row['Handling'] = implode(' ', $tools);
-
-            $finished[] = $row;
+            
+            $finished[ $error_id ] = $row;
         }
-        $html[] = $tablemkr->makeTable($finished);
+		
+	$html[] = $tablemkr->makeTable($finished);
+		
+	//render
+	echo implode('', $html);
 
-        //render
-        echo implode('', $html);
         ?>
     </div>
 </div>
---
+++ lib/phperror_class.php
@@ -0,0 +1,30 @@
+<?php
+
+class phperror {
+
+    var $error_date;
+    var $error_level;
+    var $error_msg;
+    var $error_location;
+    var $error_file;
+    var $error_line;
+    var $stack_trace_array = array();
+
+    function _construct($err_date, $err_lvl, $err_msg, $err_loca, $err_file, $err_line) {
+        $this->error_date = $err_date;
+        $this->error_level = $err_lvl;
+        $this->error_msg = $err_msg;
+        $this->error_location = $err_loca;
+        $this->error_file = $err_file;
+        $this->error_line = $err_line;
+    }
+
+    function add_strack_trace($stack_trace){
+        array_push($this->stack_trace_array, $stack_trace);
+    }
+            
+    function add_to_DB() {
+        //this should call a DB function in DB class that will handle the DB inserts
+    }
+
+}
