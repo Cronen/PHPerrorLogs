a57ab59effc3f8a07d6951d995a4112b1333bdc5
MCVDIEU <mathiascvdieu@hotmail.com>
2017-11-17 13:45:57 +0100

    tilføjet ajax trigger på sorteringsknap

--- Pages/overview.php
+++ Pages/overview.php
@@ -3,74 +3,14 @@
 <div class="container-fluid">
     <div class="col-md-12">
         <span>Vælg at sortere efter:</span>
-        <button>Level</button>
-        <button>Dato</button>
-        <button>Site</button>
-        <button>Vis alt</button>
+        <button data-state="ready" date-action="pro_sort_level" onclick="pro_sort_level()"class="btn-primary">Level</button>
+        <button data-state="ready" date-action="pro_sort_date" onclick="pro_sort_dato()" class="btn-primary">Dato</button>
+        <button data-state="ready" date-action="pro_sort_site" onclick="pro_sort_site())" class="btn-primary">Site</button>
+        <button data-state="ready" date-action="pro_sort_all" onclick="pro_sort_all()" class="btn-primary">Vis alt</button>
     </div>
 </div>
 
 <div class="container-fluid overview-table">
-    <div class="col-md-12">
-        <?php
-        //Instantiering af klasser
-        $data = new db_md();
-        $tablemkr = new table_md_class();
-
-        // Hent alle php_errors og tilføj til array
-        $sql = "SELECT * FROM php_error";
-        $arrays = $data->makeArray($sql);
-
-        //gruppér på error_id
-        $erlvs = array();
-        foreach($arrays as $array) 
-        {
-            $erlvs[ $array['error_ID'] ][] = $array;
-        }
-
-        //indhent data ud fra sorteringsvalg
-        $table_sql = "
-                SELECT 
-                '' AS 'Handling',
-                error_ID AS ID,
-                error_date AS Dato,
-                php_error_level AS level,
-                error_msg AS Fejlmeddelse,
-                error_location AS URL,
-                error_file AS Fil,
-                error_line AS linje
-                FROM php_error";
-
-        //Lav tabel med indhentet data
-        $table_data = $data->makeArray($table_sql);  
-        
-        $finished = array();
-        foreach($table_data as $array) 
-        {
-            //Har navngivet error_id til ID, derfor bruger jeg her 'ID'
-            $error_id = $array['ID'];
-            $row = $array;
-            
-            //Array med handlinger/tools/triggers
-            $tools = array();
-            
-            //"slet postering" trigger
-            $tools[] = '<button data-state="ready" data-action="pro_delete" onclick="pro_delete($(this), \'php_error\',  '.$error_id.')" class="btn-danger">Slet</button>';
-            //"Udskyd postering" trigger
-            $tools[] = '<button data-state="ready" data-action="" onclick="pro_postpone($(this), \'php_error\',  '.$error_id.')" class="btn-warning">Udskyd</button>';
-            //"Godkend postering" trigger
-            $tools[] = '<button data-state="ready" data-action=""onclick="pro_approve($(this), \'php_error\',  '.$error_id.')" class="btn-success">Godkend</button>';
-            
-            $row['Handling'] = implode(' ', $tools);
-            
-            $finished[ $error_id ] = $row;
-        }
-		
-	$html[] = $tablemkr->makeTable($finished);
-		
-	//render
-	echo implode('', $html);
-        
-        ?>
+    <div id="test" class="col-md-12">
     </div>
 </div>
--- includes/ajax/pro_handling_ajax.php
+++ includes/ajax/pro_handling_ajax.php
@@ -7,33 +7,95 @@ date_default_timezone_set("Europe/Copenhagen");
 
 //required files
 require_once('../../protected/configuration.php');
-require_once(rp_self.'lib/header_class.php');
+require_once(rp_self . 'lib/header_class.php');
 
 //load bibliotek
 Header_md::loadFiles();
 
 //Validate user exists in session
-//if (!$_SESSION['logged_in'] == true) {
-//    echo "Der en fejl. Log ind igen";
-//}
+if (!$_SESSION['logged_in'] == true) {
+   echo "Der en fejl. Log ind igen";
+}
 
 /*
  * * pro_delete
  */
 
 if (isset($_REQUEST['action']) && $_REQUEST['action'] == "pro_delete") {
-    
+
     $sqlDelete = "DELETE FROM php_error WHERE error_ID = '" . $_REQUEST['tbl_id'] . "'";
     $delete = new db_md();
     $delRes = $delete->addData($sqlDelete);
 
-    if ($delRes == true)
-    {
+    if ($delRes == true) {
         echo "Postering er slettet";
-    }
-    else
-    {
+        
+    } 
+    else {
         echo "Handlingen fejlede. Prøv igen";
-    }       
+    }
+}
+
+if (isset($_REQUEST['action']) && $_REQUEST['action'] == "pro_sort_all") {
+
+    $_SESSION['last_sort'] = "pro_sort_all";
+    //Instantiering af klasser
+    $data = new db_md();
+    $tablemkr = new table_md_class();
+
+    // Hent alle php_errors og tilføj til array
+    $sql = "SELECT * FROM php_error";
+    $arrays = $data->makeArray($sql);
+
+    //gruppér på error_id
+    $erlvs = array();
+    foreach ($arrays as $array) {
+        $erlvs[$array['error_ID']][] = $array;
+    }
+
+    //indhent data ud fra sorteringsvalg
+    $table_sql = "
+                SELECT 
+                '' AS 'Handling',
+                error_ID AS ID,
+                error_date AS Dato,
+                php_error_level AS level,
+                error_msg AS Fejlmeddelse,
+                error_location AS URL,
+                error_file AS Fil,
+                error_line AS linje
+                FROM php_error
+                LIMIT 5;";
+
+    //Lav tabel med indhentet data
+    $table_data = $data->makeArray($table_sql);
+
+    $finished = array();
+    foreach ($table_data as $array) {
+        //Har navngivet error_id til ID i mit sqlkald, derfor bruger jeg her 'ID'
+        $error_id = $array['ID'];
+        $row = $array;
+
+        //Array med handlinger/tools/triggers
+        $tools = array();
+
+        //"slet postering" trigger
+        $tools[] = '<button data-state="ready" data-action="pro_delete" onclick="pro_delete($(this), \'php_error\',  ' . $error_id . ')" class="btn-danger">Slet</button>';
+        //"Udskyd postering" trigger
+        $tools[] = '<button data-state="ready" data-action="" onclick="pro_postpone($(this), \'php_error\',  ' . $error_id . ')" class="btn-warning">Udskyd</button>';
+        //"Godkend postering" trigger
+        $tools[] = '<button data-state="ready" data-action=""onclick="pro_approve($(this), \'php_error\',  ' . $error_id . ')" class="btn-success">Godkend</button>';
+
+        $row['Handling'] = implode(' ', $tools);
+
+        $finished[$error_id] = $row;
+    }
+
+    $html[] = $tablemkr->makeTable($finished);
+
+    //render
+    echo implode('', $html);
+    
+    
 }
 ?>
