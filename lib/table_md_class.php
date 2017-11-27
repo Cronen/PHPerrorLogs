32c78e602e75e5104f57bfc49668e4fa1dfcad15
Thomas Crone Larsen <thom802b@edu.eal.dk>
2017-11-27 09:21:13 +0100

    Ændret således table tr for korrekt ID

--- lib/table_md_class.php
+++ lib/table_md_class.php
@@ -1,69 +1,60 @@
 <?php
 
-class table_md_class
-{
-	public $id, $use_index_as_id;
-	
-	function __construct($id = NULL)
-	{
-		$this->id = $id;
-		$this->use_index_as_id = false; //uses index as postfix id on row
-	}
-	
-	function makeTable($arrays)
-	{                
-		if(is_null($arrays))
-			return false;
-		
-		$rows = array();
-		
-		//make header row
-		foreach($arrays as $array)
-		{
-			$header = array_keys($array);
-			break;
-		}
-			
-		$rows[] = $this->makeTableHead($header);
-		
-		//make rows
-		foreach($arrays as $index => $array)
-			$rows[] = $this->makeRow($array, $index);
-		
-		$html = '<table class="table table-responsive">'.implode('', $rows).'</table>';
-		
-		return $html;
-	}
+class table_md_class {
 
-	function makeRow($array, $index)
-	{
-		$cells = array();
-		foreach($array as $key => $value)
-		{
-			$cells[] = '<td >'.$value.'</td>';
-		}
-		
-		$id = ($this->use_index_as_id && !is_null($this->id))
-			? $this->id.'-'.$index
-			: NULL;
-			
-		$row = '<tr id="'.$id.'">'.implode('', $cells).'</tr>';
-		
-		return $row;
-	}
-	
-	function makeTableHead($array)
-	{
-		$cells = array();
-		foreach($array as $value)
-		{
-			$cells[] = '<th >'.$value.'</th>';
-		}
-		
-		$row = '<tr>'.implode('', $cells).'</tr>';
-		
-		return $row;
-	}
+    public $id, $use_index_as_id;
+
+    function __construct($id = NULL) {
+        $this->id = $id;
+        $this->use_index_as_id = true; //uses index as postfix id on row
+    }
+
+    function makeTable($arrays) {
+
+        if (is_null($arrays))
+            return false;
+
+        $rows = array();
+
+        //make header row
+        foreach ($arrays as $array) {
+            $header = array_keys($array);
+            break;
+        }
+
+        $rows[] = $this->makeTableHead($header);
+
+        //make rows
+        foreach ($arrays as $index => $array)
+            $rows[] = $this->makeRow($array, $index);
+
+        $html = '<table class="table table-responsive">' . implode('', $rows) . '</table>';
+
+        return $html;
+    }
+
+    function makeRow($array, $index) {
+        $cells = array();
+        foreach ($array as $key => $value) {
+            $cells[] = '<td >' . $value . '</td>';
+        }
+        $id = ($this->use_index_as_id) ? $this->id . '' . $index : NULL;
+        $row = '<tr id="' . $id . '">' . implode('', $cells) . '</tr>';
+
+        return $row;
+    }
+
+    function makeTableHead($array) {
+        $cells = array();
+        foreach ($array as $value) {
+            $cells[] = '<th >' . $value . '</th>';
+        }
+
+        $row = '<tr>' . implode('', $cells) . '</tr>';
+
+        return $row;
+    }
+
 }
 
 ?>
