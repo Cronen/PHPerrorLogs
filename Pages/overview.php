89f79a1e945f3ce2a0b0796fa0517c49b01b56cf
Thomas Crone Larsen <thom802b@edu.eal.dk>
2017-12-11 09:45:35 +0100

    Små ændringer til overview

--- Pages/overview.php
+++ Pages/overview.php
@@ -1,18 +1,15 @@
 <?php
-
 //Instantiering til dashboard_class.php
 //$overview = new dashboard_func();
-
 //Simuler logout - Mest for proof of concept. Regner med at du har en selv.
-if (isset($_REQUEST['logout_btn']))
-{
+if (isset($_REQUEST['logout_btn'])) {
     session_destroy();
     header("Location: index.php");
 }
 ?>
 
 <div class="container-fluid">
-    
+
     <div class="dash-board">
         <div class="dashboard-sort col-md-8">
             <h3>Vælg at sortere efter:</h3>
@@ -20,27 +17,29 @@ if (isset($_REQUEST['logout_btn']))
             <button data-action="asc" onclick="pro_sort_date($(this));" class="btn-primary sort-button">Dato</button>
             <button data-action="asc" onclick="pro_sort_location($(this));" class="btn-primary sort-button">Site</button>
         </div>
-        
+
         <div class="dashboard-script col-md-4">
             <form>
                 <span>Logget ind som: 
-                 <?php
-                    if(isset($_SESSION['user_name']))
-                    { 
-                        echo $_SESSION['user_name'];
-                    }
-                 ?>
+<?php
+if (isset($_SESSION['user_name'])) {
+    echo $_SESSION['user_name'];
+}
+?>
                     <button name="logout_btn" class="btn-info btn-xs glyphicon glyphicon-off" value="Logout"></button>
                 </span>
-                    <br>
-                <span>
-                    <b>Scriptet er sidst kørt: </b>
-                    <span id="script-time"></span>
-                    <button data-state="ready" onclick="run_script();" class="btn-xs btn-info" id="refreshbtn"><span class="glyphicon glyphicon-refresh"></span></button>
-                </span>
-                    <br>
-                 <button class="btn-info" onclick="">Se scriptlog</button>
+                <br>
+
             </form>
+            <span>
+                <b>Scriptet er sidst kørt: </b>
+                <span id="script-time"></span>
+                <button data-state="ready" onclick="run_script()" class="btn-xs btn-info" id="refreshbtn"><span class="glyphicon glyphicon-refresh"></span></button>
+                <br>
+                <div id="last_run_div"></div>
+            </span>
+            <br>
+            <button class="btn-info" onclick="pro_scriptlog()">Se scriptlog</button>
         </div>
     </div>
 </div>
@@ -49,10 +48,10 @@ if (isset($_REQUEST['logout_btn']))
 
 <!--Div der hentes tables ind i -->
 <div class="content-wrapper col-md-12">
-    
+
 </div>
 
 <!-- Modal -->
 <div id="postponeModal" class="modal fade" role="dialog">
-  
+
 </div>
